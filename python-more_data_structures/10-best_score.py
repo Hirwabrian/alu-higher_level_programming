e2850cb86be57ca6223634648a293df1eb690893
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-02-29 18:39:47 +0200

    Update 10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if len(a_dictionary) == 0:
         return None
     j = list(a_dictionary.keys())[0]
-    big = a_dictionary[ret]
+    big = a_dictionary[j]
     for k, v in a_dictionary.items():
         if v > big:
             big = v
