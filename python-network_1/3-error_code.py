7b2cc74a5b073304da8f5bd6f2932a4ccfac865d
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-03-24 13:00:31 +0200

    3-error_code.py

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(request) as response:
             print(response.read().decode("ascii"))
-    except HTTPError as e:
+    except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
