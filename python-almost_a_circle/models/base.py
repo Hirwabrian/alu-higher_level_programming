77f0b952186fb8e36a5f072b7f59fe729ddb5424
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-04-06 21:08:52 +0200

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -27,17 +27,6 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    def __init__(self, id=None):
-        """Initialize a new Base.
-
-        Args:
-            id (int): The identity of the new Base.
-        """
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
