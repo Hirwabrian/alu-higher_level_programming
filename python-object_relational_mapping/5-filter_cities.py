8f7fa1bcd94447ff5fc58beae175e4d6bc47aefe
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-04-13 19:28:40 +0200

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
                 INNER JOIN `states` as `s` \
                    ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
-    print(", ".join([city[2] \
+    print(", ".join([city[2] 
                      for city in c.fetchall() if city[4] == sys.argv[4]]))
