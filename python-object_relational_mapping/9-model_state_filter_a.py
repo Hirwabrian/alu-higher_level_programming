f3d21ea5bbef7077381febd97250efa15af8c54f
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-04-13 23:10:35 +0200

    9-model_state_filter_a.py

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
+    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all(
 
     for state in session.query(State).order_by(State.id):
-        if "a" in state.name:
-            print("{}: {}".format(state.id, state.name))
+        print("{}: {}".format(state.id, state.name))
