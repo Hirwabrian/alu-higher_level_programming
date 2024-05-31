bbc42adf50698635f09446f266f73d28e0d51069
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-05-31 21:56:08 +0200

    Update 10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-function factorial(n) {
-    if (isNaN(n) || n <= 1) {
-        return 1;
-    }
-    return n * factorial(n - 1);
+function factorial(n) {if (isNaN(n) || n <= 1) {return 1;}return n * factorial(n - 1);
 }
 console.log(factorial(Number(process.argv[2])));
