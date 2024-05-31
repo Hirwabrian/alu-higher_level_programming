b406d6362320bba97c469b1f37253d3880ebb626
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-05-31 21:48:21 +0200

    5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const num = Math.floor(Number(process.argv[2]));
-if (!isNaN(num)){
-	console.log(`My number: ${num}`);
+if (!isNaN(num)) {
+console.log(`My number: ${num}`);
 }
-else {
-	console.log('Not a number')
+else {console.log('Not a number');
 }
