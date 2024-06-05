db80a7928629dc6461b749d753dddf3509614891
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-06-05 14:30:37 +0200

    4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request(url, function (err, data, body) {
+
+request(url, function (err, response, body) {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
     let counter = 0;
     const films = JSON.parse(body).results;
     films.forEach(film => {
       const characters = film.characters;
-      if (characters.includes(`https://swapi-api.alx-tools.com/api/people/18/`)) {
-        count++;
+      if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        counter++;
       }
     });
-    }
     console.log(counter);
   }
-);
+});
